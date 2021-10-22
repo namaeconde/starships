45bc6e56bb6868a891071ed8c8c09f47b5c01b06
Namae Conde <lunamae.conde@sensis.com.au>
2021-10-22 16:56:11 +1100

    Refactor header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,5 @@
-import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { color } from '../theme';
 
 export const StarshipLogo = () => {
   return (
@@ -10,28 +7,11 @@ export const StarshipLogo = () => {
   )
 }
 
-export default function Header({ fixedHeader }: { fixedHeader: Boolean}) {
-  const position = fixedHeader ? "fixed" : "static";
+export default function Header() {
 
   return (
-    <AppBar elevation={0} position={position} style={{ backgroundColor: color.black_pearl }}>
-      <Box 
-        height={80} 
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        pl={{ xs: 2, sm: 3 }}
-        pr={{ xs: 2, sm: 3 }}>
-        <StarshipLogo />
-        <Button
-          style={{ 
-            color:`${color.wild_watermelon}`,
-            backgroundColor:`${color.racing_green}`,
-            borderRadius:"20px"
-          }}>
-            View Favorites
-        </Button>
-      </Box>
-    </AppBar>
+    <>
+      <StarshipLogo />
+    </>
   )
 }
