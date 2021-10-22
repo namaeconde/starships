34febb2c5f7045ad419e90b03405d3c2b74e4fcb
Namae Conde <lunamae.conde@sensis.com.au>
2021-10-22 20:19:08 +1100

    Update starship card color and border radius

--- src/components/StarshipCard.tsx
+++ src/components/StarshipCard.tsx
@@ -1,15 +1,16 @@
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Rating from '@mui/material/Rating';
+import {color} from '../theme'
 
 export default function StarshipCard({ starship }: { starship: any}): JSX.Element { //TODO: Update starship any to Starship Interface
   const { name, manufacturer, hyperdrive_rating, passengers } = starship;
 
   return (
-    <Paper sx={{ p:3 }}>
+    <Paper sx={{ p:3, backgroundColor:`${color.midnigt_moss}`, borderRadius:2 }}>
       <Typography fontSize={24} fontWeight={700}>{name}</Typography>
       <Typography noWrap fontSize={18}>{manufacturer}</Typography>
-      <Rating name="read-only" value={hyperdrive_rating} readOnly />
+      <Rating name="read-only" value={Number(hyperdrive_rating)} readOnly />
       <Typography fontSize={18}>Passengers: {passengers}</Typography>
     </Paper>
   )
