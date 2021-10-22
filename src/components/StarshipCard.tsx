bbf0ea6ce59a0d0948b7de1d5447f7af871639b6
Namae Conde <lunamae.conde@sensis.com.au>
2021-10-22 23:27:00 +1100

    Add favorite button and function

--- src/components/StarshipCard.tsx
+++ src/components/StarshipCard.tsx
@@ -1,17 +1,50 @@
-import Paper from "@mui/material/Paper";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Rating from '@mui/material/Rating';
-import {color} from '../theme'
+import {color} from '../theme';
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import StarshipImage from '../images/starship.png';
+import Grid from "@mui/material/Grid";
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import { styled } from '@mui/material/styles';
 
-export default function StarshipCard({ starship }: { starship: any}): JSX.Element { //TODO: Update starship any to Starship Interface
+const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}));
+
+export default function StarshipCard({ starship }: { starship: any}): JSX.Element { // TODO: Update starship any to Starship Interface
   const { name, manufacturer, hyperdrive_rating, passengers } = starship;
+  const [ isFavorite, setIsFavorite ] = useState(false); // TODO: Update to get data from redux
 
   return (
-    <Paper sx={{ p:3, backgroundColor:`${color.midnigt_moss}`, borderRadius:2 }}>
-      <Typography fontSize={24} fontWeight={700}>{name}</Typography>
-      <Typography noWrap fontSize={18}>{manufacturer}</Typography>
-      <Rating name="read-only" value={Number(hyperdrive_rating)} precision={0.5} readOnly />
-      <Typography fontSize={18}>Passengers: {passengers}</Typography>
-    </Paper>
+    <Box sx={{ p:3, backgroundColor:`${color.midnigt_moss}`, borderRadius:2 }}>
+      <Box container component={Grid}>
+        <Box item xs={8} component={Grid}>
+          <LightTooltip title={name}>
+            <Typography noWrap fontSize={24} fontWeight={700}>{name}</Typography>
+          </LightTooltip>
+          <LightTooltip title={manufacturer}>
+          <Typography noWrap fontSize={18}>{manufacturer}</Typography>
+          </LightTooltip>
+          <Rating name="read-only" value={Number(hyperdrive_rating)} precision={0.5} readOnly />
+          <Typography fontSize={18}>Passengers: {passengers}</Typography>
+        </Box>
+        <Box item xs={4} sx={{ backgroundImage: `url(${StarshipImage})`, borderRadius:2 }} component={Grid}>
+          <IconButton color="primary" sx={{ float:"right" }} onClick={() => setIsFavorite(!isFavorite)}>
+            { isFavorite ? <FavoriteIcon/> : <FavoriteBorderIcon/>}
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
   )
 }
