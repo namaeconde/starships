0c3b23acd0e09a67b199a890de583ed363d07920
Namae Conde <lunamae.conde@sensis.com.au>
2021-10-23 22:34:54 +1100

    Update App unit test and snapshot

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+const initialState = { favoriteList: { value: [] } }
+const mockStore = configureStore()
+let store
 
 test('App renders', () => {
-  const component = renderer.create(<App />,);
+  store = mockStore(initialState);
+  const component = renderer.create(<Provider store={store}><App /></Provider>,);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
