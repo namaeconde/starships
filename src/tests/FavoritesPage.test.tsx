a1744a84d478f42f7bf19665e984674a3d1b23cd
Namae Conde <lunamae.conde@sensis.com.au>
2021-10-23 22:47:42 +1100

    Add FavoritesPage unit test and snapshot

--- src/tests/FavoritesPage.test.tsx
+++ src/tests/FavoritesPage.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import HomePage from '../pages/HomePage';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import FavoritePage from '../pages/FavoritesPage';
 
 const initialState = { favoriteList: { value: [] } }
 const mockStore = configureStore()
 let store
 
-test('HomePage renders', () => {
+test('FavoritesPage renders', () => {
   store = mockStore(initialState);
-  const component = renderer.create(<Provider store={store}><Router><HomePage /></Router></Provider>,);
+  const component = renderer.create(<Provider store={store}><Router><FavoritePage /></Router></Provider>,);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
